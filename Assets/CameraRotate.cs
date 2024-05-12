16983b6af05d3241121f3c77bf018c93672d7df0
YarTom <yartomilin@mail.ru>
2024-05-12 17:59:57 +0300

    da pofigu

--- Assets/CameraRotate.cs
+++ Assets/CameraRotate.cs
@@ -1,10 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRotate : MonoBehaviour
 {
-    public Transform player;
+    public List<Transform> player;
+    private int i;
     void Update()
     {
-        transform.LookAt(player.position);
+        transform.LookAt(player[i].position);
+
+        if (Input.GetKeyDown(KeyCode.Tab)) 
+        {
+            i++; 
+            if (i > player.Count - 1)
+            {
+                i = 0;
+            }
+        }
     }
 }
--- Assets/PlayerConttroller.cs
+++ Assets/PlayerConttroller.cs
@@ -1,38 +1,36 @@
 using UnityEngine;
+using System;
 
 public class PlayerConttroller : MonoBehaviour
 {
     private Rigidbody rb;
+    private CharacterController ch;
 	public float Speed;
     public float Gravity;
     public float JumpSpeed;
     public float GravityVelocity;
 
     public bool IsMirrorClone;
-    void Start()
+
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
         if(IsMirrorClone)
         {
             Speed *= -1;
         }
     }
-    //private void Update()
-    //{
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-           // Gravity = JumpSpeed;
-        //}
-        
-    //}
     void FixedUpdate()
     {
         var horiontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
-        rb.velocity = new Vector3(horiontal * Speed * Time.fixedDeltaTime, Gravity, vertical * Speed * Time.fixedDeltaTime);
+        rb.velocity = new Vector3(horiontal * Speed * Time.fixedDeltaTime,Time.fixedDeltaTime * Gravity, vertical * Speed * Time.fixedDeltaTime);
         if (horiontal != 0 || vertical != 0)
         {
-            transform.rotation = Quaternion.LookRotation(new Vector3(horiontal * Time.fixedDeltaTime, 0, vertical * Time.fixedDeltaTime));
+            transform.rotation = Quaternion.LookRotation(new Vector3(Math.Sign(Speed) * horiontal * Time.fixedDeltaTime, 0, Math.Sign(Speed) * vertical * Time.fixedDeltaTime));
         }
     }
     
