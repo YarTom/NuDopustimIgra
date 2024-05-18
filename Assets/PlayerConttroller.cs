39fd0ad96adec629c60ccb4e91645855aab0dc17
YarTom <yartomilin@mail.ru>
2024-05-18 17:00:48 +0300

    Physics fix

--- Assets/PlayerConttroller.cs
+++ Assets/PlayerConttroller.cs
@@ -10,6 +10,7 @@ public class PlayerConttroller : MonoBehaviour
     public float Gravity;
     public float JumpSpeed;
     public float GravityVelocity;
+    public CapsuleCollider BoxCollider;
 
     // My
     private float _fallVelocity = 0;
@@ -21,6 +22,7 @@ public class PlayerConttroller : MonoBehaviour
 
     private void Awake()
     {
+        BoxCollider = GetComponent<CapsuleCollider>();
         _an = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody>();
         if (IsMirrorClone)
@@ -28,33 +30,22 @@ public class PlayerConttroller : MonoBehaviour
             Speed *= -1;
         }
     }
-
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    isGrounded = true;
-    //}
-
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    isGrounded = false;
-    //}
-
     void FixedUpdate()
     {
-        //_fallVelocity += Gravity * Time.fixedDeltaTime;
-        //if (isGrounded)
-        //{
-        //    _fallVelocity = 0;
-        //}
-
-        //if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
-
-        //    _fallVelocity += JumpSpeed;
-        //}
-
+        bool raycastHit = Physics.Raycast(BoxCollider.bounds.center, Vector3.down, BoxCollider.bounds.extents.y + 0.01f);
+        if (raycastHit)
+        {
+            _fallVelocity = 0;
+            isGrounded = true;
+        }
+        else
+        {
+            _fallVelocity += Gravity * Time.fixedDeltaTime;
+            isGrounded = false;
+        }
         var horiontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
-        rb.velocity = new Vector3(horiontal * Speed * Time.fixedDeltaTime, 0, vertical * Speed * Time.fixedDeltaTime);
+        rb.velocity = new Vector3(horiontal * Speed * Time.fixedDeltaTime, _fallVelocity * Time.fixedDeltaTime, vertical * Speed * Time.fixedDeltaTime);
         if (horiontal != 0 || vertical != 0)
         {
             transform.rotation = Quaternion.LookRotation(new Vector3(Math.Sign(Speed) * horiontal * Time.fixedDeltaTime, 0, Math.Sign(Speed) * vertical * Time.fixedDeltaTime));
@@ -65,6 +56,4 @@ public class PlayerConttroller : MonoBehaviour
             _an.SetBool("IsRun", false);
         }
     }
-    
-    
 }
